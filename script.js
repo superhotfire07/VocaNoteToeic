ba77682db76eb21e165e8ba899c0a2e90183620e
TJWGAL <53142620+superhotfire07@users.noreply.github.com>
2021-04-11 23:36:57 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,67 +2,25 @@ var searchBox = $('#searchBox');
 var submitBtn = $('button');
 var tr = $('tr');
 var AreThereAnyResults = false;
-
+var td = $('td');
+$('#noResult').hide(0);
 
 
 $(document).ready(
     function() {
-        $(document).submit(
+        searchBox.keyup(
             function(e) {
-            e.preventDefault();
+            $('#noResult').hide(0);
+            var $keyword = $(this).val().toLowerCase();
+            $('tbody > tr').hide(0);
+            var tdVal = $('td:contains('+$keyword+')');
+            tdVal.parent().show(0);
             
-            
-            var td = tr.children();
-
-            td.each (function(i){
-
-            var realTd = $(this).text().replace(/(\s*)/g, "");
-
-
-            var boxVal = searchBox.val().toLowerCase().replace(/(\s*)/g, "");
-
-            
-            if (realTd == boxVal) {
-                /* $('td').hide(0);
-                $('tbody > tr > th').hide(0);
-                
-                
-                $(this).parent().children().show(0); */
-                
-             
-
-                if (AreThereAnyResults == false) {
-                var result = $(this).parent().clone().addClass('bg-light').attr('data-bs-toggle','modal').attr('data-bs-target','#delete');
-                $('tbody').prepend(result);
-                AreThereAnyResults = true;
-
-                } else{
-                    console.log($('tbody tr').eq(0).text());
-                $('tbody tr').eq(0).remove();
-               
-                var result = $(this).parent().clone().addClass('bg-light').attr('data-bs-toggle','modal').attr('data-bs-target','#delete');
-                $('tbody').prepend(result);
-                
-                }
-               
-                return false;
-            } 
-            else {
-                return true;
-                
+            if (typeof tdVal.css('display') == 'undefined'){
+                $('#noResult').show(0);
             }
-        });
-        $('html, body').animate({scrollTop: '0'}, 50);
-        
-       
     }
 );
 });
 
-$('.delete').click(function() {
-    console.log('hello');
-    $('.bg-light').eq(0).remove();
-    $('.modal').modal('hide');
-    AreThereAnyResults=false;
-});
 
